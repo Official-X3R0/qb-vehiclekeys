2a95e7465fb3373885da65ee0e4fe40ccb72bb5a
X3R0 <69587282+Official-X3R0@users.noreply.github.com>
2024-07-21 11:42:47 +0200

    Add files via upload

---
+++ skillchecks/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version "cerulean"
+
+games { "gta5" }
+
+description "NoPixel Boilerplate"
+
+version "0.1.0"
+
+ui_page 'nui/dist/index.html'
+
+files {
+    "nui/dist/**/*",
+    "nui/dist/*"
+
+}
+
+client_scripts {
+    "client/*"
+}
